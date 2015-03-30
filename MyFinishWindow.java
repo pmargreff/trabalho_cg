ecd55ec1fa7a2ab0c41a458272788336ef915333
Pablo Margreff <pablo_margreff@hotmail.com>
2015-03-30 01:02:39 -0300

    altaracao classe MyFinishWindow

--- MyFinishWindow.java
+++ MyFinishWindow.java
@@ -1,11 +1,5 @@
 import java.awt.event.*;
 
-/**
-* This class is for enabling the closing of a window.
-*
-* @author Frank Klawonn
-* Last change 07.01.2005
-*/
    public class MyFinishWindow extends WindowAdapter
    {
      public void windowClosing(WindowEvent e)
