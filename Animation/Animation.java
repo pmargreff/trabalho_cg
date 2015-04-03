6b0273307e6514f62182586880800ab05d95fd27
Bruno G. Pinto <bgpinto@inf.ufpel.edu.br>
2015-04-03 00:42:17 -0300

    Estrutura inicial do calculo da cena criado. Alguns bugs como: o quadrado nao aparece;

--- Animation/Animation.java
+++ Animation/Animation.java
@@ -16,19 +16,19 @@ import Point.Point; // eu odeio java...
 */
 
 
-//public class Animation extends Frame
-public class Animation {
-
+public class Animation extends Frame {
   //Construtor
-  public Animation(  int r, int s, int n  ) {
+  public Animation(  int r, int s, int n, int x, int y ) {
 
     this.radius      = r;
     this.segments    = s;
     this.repetitions = n;
+    this.x_0         = x;  
+    this.y_0         = y;  
 
     point_list = calculateBrasenham();
 
-    //addWindowListener(new MyFinishWindow());
+    addWindowListener(new MyFinishWindow());
     
   }
 
@@ -44,7 +44,7 @@ public class Animation {
       int y = radius;
       int decision_var = 1 - radius;
       
-      rigth_half.add( new Point(x, y) ); // eu acho que tem que ter ponto negativo.
+      rigth_half.add( new Point(x , y  ) ); // eu acho que tem que ter ponto negativo.
 
       while ( y > x ) {
 
@@ -58,7 +58,7 @@ public class Animation {
           y--;
         }
 
-        rigth_half.add( new Point(x , y ) );  // entao ..      
+        rigth_half.add( new Point(x , y ) );      
       }
 
       // NOTE: Aparece um "erro", ou seja alguns pontos retrocedem e nao sao coerentes;
@@ -84,6 +84,14 @@ public class Animation {
       final_list.addAll(rigth_half);
 
 
+      for (int i = 0; i < final_list.size(); i++ ) {
+
+          Point tmp = final_list.get(i);
+          final_list.get(i).set_x(tmp.get_x() + x_0 + radius);
+          final_list.get(i).set_y(tmp.get_y() + y_0);
+      }
+
+
       return final_list;
 
   }
@@ -98,82 +106,149 @@ public class Animation {
 
   }
 
-  // public void paint(Graphics g)
-  // {
 
-  //   Graphics2D g2d = (Graphics2D) g;
-  //   //Ativa o antialiasing
-  //   g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING,RenderingHints.VALUE_ANTIALIAS_ON);
+// agora a giripoca vai pia
 
+// Ok, vai se lavar. vou mijar e comer; voltamos ok; avisa no fb
+public void paint(Graphics g) {
 
-  //   int coordIniX, coordIniY, tamanhoQuad, tamanhoInicial, tamanhoFinal;
-  //   tamanhoInicial = 10;
-
-  //   coordIniX = 300;     //coordenada inicial no X
-  //   coordIniY = 150;     //coordenada inicial do Y
-  //   tamanhoQuad = tamanhoInicial;  //tamanho de quadrado recebe o tamanho inicial
-  //   //cria a elipse de acordo com as coordenadas iniciais e tamanho
-  //   Rectangle2D.Double quad = new Rectangle2D.Double(coordIniX,coordIniY,tamanhoQuad,tamanhoQuad);
+    Graphics2D g2d = (Graphics2D) g;
+    //Ativa o antialiasing
+    g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING,RenderingHints.VALUE_ANTIALIAS_ON);
 
 
-  //   //Ponto médio dos lados do quadrado
-  //   //é usado para definir o ponto central para a rotação
-  //   double xMed = coordIniX+tamanhoQuad/2.0;
-  //   double yMed = coordIniY+tamanhoQuad/2.0;
+    // ver deposi ...
+    int coordIniX, coordIniY, tamanhoFinal;
+  
+
+    coordIniX = x_0 - radius;       //coordenada inicial no X
+    coordIniY = y_0;                //coordenada inicial do Y
+    //cria a elipse de acordo com as coordenadas iniciais e tamanho
+    Rectangle2D.Double quad = new Rectangle2D.Double(coordIniX,coordIniY,10 , 10);
 
 
-  //   //Cria a rotação inical 0 graus a partir do ponto central.
-  //   AffineTransform transfInicial = new AffineTransform();
-  //   transfInicial.setToRotation(0,xMed,yMed); //rotação inicial
-
-  //   //Cria e o objeto inicial
-  //   double[] matrizInicial = new double[6];
-  //   transfInicial.getMatrix(matrizInicial);
+    //Ponto médio dos lados do quadrado
+    //é usado para definir o ponto central para a rotação
+    // ver depois
 
 
-  //   //objetofinal
-  //   AffineTransform transfFinal = new AffineTransform();
-  //   transfFinal.setToTranslation(0,0); //posicao final, será um vetor
-  //   transfFinal.concatenate(mudaTamanho(xMed,yMed,1.5,1.5));//faz a mudança da escaĺa
-  //   transfFinal.rotate(Math.PI/4,xMed,yMed); //faz a rotação do objeto
-
-  //   //matriz para a transformação final
-  //   double[] matrizFinal = new double[6];
-  //   transfFinal.getMatrix(matrizFinal);
+    //Cria a rotação inical 0 graus a partir do ponto central.
+    //AffineTransform transfInicial = new AffineTransform();
+    //transfInicial.setToRotation(0,xMed,yMed); //rotação inicial
 
 
-  //   //Shape usado para aplicar a transformação.
-  //   Shape s;
+
+    //objetofinal
+    // AffineTransform transfFinal = new AffineTransform();
+    // transfFinal.setToTranslation(0,0); //posicao final, será um vetor
+    // transfFinal.concatenate(mudaTamanho(xMed,yMed,1.5,1.5));//faz a mudança da escaĺa
+    // transfFinal.rotate(Math.PI/4,xMed,yMed); //faz a rotação do objeto
 
 
-  //   //objeto que irá receber todos os passos intermediários da transformação
-  //   AffineTransform transfIntermed;
 
-  //   double passos = 200; //Number of passos as a Double-value in order 
-  //                               //to avoid repeated casting in the loop.
-  //   for (double i=0; i<=passos; i++)
-  //   {
-  //     //calcula a etapa da interpolação
-  //     transfIntermed = new AffineTransform(
-  //                 interpola(matrizInicial,matrizFinal,i/passos));
 
-  //     //transforma a o quadrado para o tipo shape, para poder aplicar as operações nessárias
-  //     s = transfIntermed.createTransformedShape(quad);
+   // double passos = 200; //Number of passos as a Double-value in order 
+                                //to avoid repeated casting in the loop.
 
-  //     //delay para redesenhar
-  //     congela(50);
 
-  //     limpaJanela(g2d);
+// ---------------------------------------------------------------------
 
-  //     //redesenha
-  //     g2d.fill(s);
-  //   }
-  // }
+
+    double xMed = coordIniX + 5;
+    double yMed = coordIniY + 5;
+
+    int jump = (point_list.size() / segments);
+
+
+    //Shape usado para aplicar a transformação.
+    Shape s;
+
+
+    //objeto que irá receber todos os passos intermediários da transformação
+    AffineTransform transfIntermed;
+
+    for ( int r = 0; r < repetitions; r++ ) {
+
+      for (int j = 0; j < segments; j++) {
+
+
+      int x_jump = point_list.get(j * jump).get_x();  
+      int y_jump = point_list.get(j * jump).get_y();  
+
+      AffineTransform rotation  = new AffineTransform(); // realmente preciso iniciar aqui?
+      AffineTransform translate = new AffineTransform();
+      translate.setToTranslation(x_jump , y_jump);
+
+      if ( (j % 2) == 0 )  {
+        rotation.setToRotation( Math.toRadians(45), xMed, yMed );
+        translate.concatenate( mudaTamanho(x_jump, y_jump, 1.5, 1.5) );
+      } else {
+        translate.setToRotation(Math.toRadians(-45), xMed, yMed );
+        translate.concatenate( mudaTamanho(x_jump, y_jump, 0.6666, 0.6666) );
+      } 
+
+
+       //Cria e o objeto inicial
+      double[] matrizInicial = new double[6];
+      rotation.getMatrix(matrizInicial);
+
+      //matriz para a transformação final
+      double[] matrizFinal = new double[6];
+      translate.getMatrix(matrizFinal);
+
+      for (double step = 0; step < 10; step++ ) {
+         //calcula a etapa da interpolação
+        transfIntermed = new AffineTransform( interpola(matrizInicial, matrizFinal , step/200) );
+
+        //transforma a o quadrado para o tipo shape, para poder aplicar as operações nessárias
+        s = transfIntermed.createTransformedShape(quad);
+
+        //delay para redesenhar
+        congela(50);
+
+        limpaJanela(g2d);
+
+        //redesenha
+        g2d.fill(s);
+      }
+
+      // talvez seja dentro do if para a atual transf;  
+      xMed = x_jump + 5;  
+      yMed = y_jump + 5;  
+
+      //point_list.get(j * jump).get_x(), point_list.get(j * jump).get_x()  
+
+      }
+        
+    }
+
+
+
+// ---------------------------------------------------------------------
+
+    // for (double i=0; i<=passos; i++)
+    // {
+    //   //calcula a etapa da interpolação
+    //   transfIntermed = new AffineTransform(
+    //               interpola(matrizInicial,matrizFinal,i/passos));
+
+    //   //transforma a o quadrado para o tipo shape, para poder aplicar as operações nessárias
+    //   s = transfIntermed.createTransformedShape(quad);
+
+    //   //delay para redesenhar
+    //   congela(50);
+
+    //   limpaJanela(g2d);
+
+    //   //redesenha
+    //   g2d.fill(s);
+    // }
+  }
 
 
   //calcula a interpolação
   // alfa sempre será estara no intervalo de [0,1]
-  public static double[] interpola(double[] inicial,double[] terminal, double alfa)
+  public double[] interpola(double[] inicial,double[] terminal, double alfa)
   {
     double[] intermed = new double[inicial.length]; //matriz que vai conter os valores váriaveis da interpolação
 
@@ -187,7 +262,7 @@ public class Animation {
 
 
   //método que limpa a janela
-  public static void limpaJanela(Graphics2D g)
+  public void limpaJanela(Graphics2D g)
   {
     g.setPaint(Color.white); //define o pincel como branco
     g.fill(new Rectangle(0,0,600,300)); //cria um retangulo preenchido da primeira cor
@@ -197,7 +272,7 @@ public class Animation {
 
   //recebe como parametros as dimenções do objeto original e as novas 
   //escalas em x e y que cada um deve receber
-  public static AffineTransform mudaTamanho(double x, double y,
+  public AffineTransform mudaTamanho(double x, double y,
                                              double novaEscalaX, double novaEscalaY)
   {
     AffineTransform mt = new AffineTransform();
@@ -210,7 +285,7 @@ public class Animation {
   }
 
   //recebe o tempo em milissegundos
-  public static void congela(long t)
+  public void congela(long t)
   {
     long finish = (new Date()).getTime() + t;
     while( (new Date()).getTime() < finish ){}
@@ -220,15 +295,8 @@ public class Animation {
   private int radius;     
   private int segments;   
   private int repetitions;
+  private int x_0, y_0;
   private ArrayList<Point> point_list;
-
-  // public static void main(String[] argv)
-  // {
-  //   Animacao tela = new Animacao();
-  //   tela.setTitle("Animation do quadrado mudando de forma por interpolacao de pontos");
-  //   tela.setSize(600,300);
-  //   tela.setVisible(true);
-  // }
 
 }
 
--- Main.java
+++ Main.java
@@ -4,20 +4,23 @@ public class Main {
 
 	public static void main(String[] argv) {
 
-		if ( argv.length == 3 ) {
+		if ( argv.length == 5 ) {
 
 			int radius 	 	= Integer.parseInt(argv[0]);
 			int segments 	= Integer.parseInt(argv[1]);
 			int repetitions = Integer.parseInt(argv[2]);
+			int x_0			= Integer.parseInt(argv[3]);
+			int y_0			= Integer.parseInt(argv[4]);
+
 
 			if ( segments < 2 ) segments = 2;
 
-			Animation scene = new Animation( radius, segments, repetitions );
+			Animation scene = new Animation( radius, segments, repetitions, x_0, y_0 );
 
-			scene.debugg();
-	    	//scene.setTitle("CG Trabalho I - Bruno e Pablo");
-	    	//scene.setSize(600,300);
-	   		//scene.setVisible(true);
+			//scene.debugg();
+	    	scene.setTitle("CG Trabalho I - Bruno e Pablo");
+	    	scene.setSize(100 + radius, (radius * 2 * repetitions) + 100);
+	   		scene.setVisible(true);
 
  
 		} else {
