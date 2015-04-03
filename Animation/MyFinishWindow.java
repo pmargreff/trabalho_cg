650d36a2170e53021de332adb5b94fa3dd4ba22c
Bruno G. Pinto <bgpinto@inf.ufpel.edu.br>
2015-04-03 15:50:12 -0300

    Preparando para arrumar as transformações

--- Animation/MyFinishWindow.java
+++ Animation/MyFinishWindow.java
@@ -2,12 +2,12 @@ package Animation;
 
 import java.awt.event.*;
 
-   public class MyFinishWindow extends WindowAdapter
-   {
-     public void windowClosing(WindowEvent e)
-     {
-       System.exit(0);
-     }
-   }
+public class MyFinishWindow extends WindowAdapter
+{
+ public void windowClosing(WindowEvent e)
+ {
+   System.exit(0);
+ }
+}
 
 
